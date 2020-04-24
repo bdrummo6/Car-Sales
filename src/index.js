b3690af7a42eec73ac38eb280adefbaa63d781ae
bdrummo6 <bradley.drummonds@gmail.com>
2020-04-23 21:44:39 -0400

    Update index.js
    Imported Provider and createStore into index.js. I created a store object and passed it to provider through props.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { Provider } from 'react-redux'; // importing Provider to wrap the App component in
+import { createStore } from 'redux'; // importing creeateStore to create a store object
+
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const store = createStore();
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
