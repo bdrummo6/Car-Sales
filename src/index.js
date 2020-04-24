f94987ad67003e40bca030ea465fbac39f87d6d8
bdrummo6 <bradley.drummonds@gmail.com>
2020-04-23 21:50:18 -0400

    Update index.js
    Imported reducer into index.js and passed it as an argument into the createStore function.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import App from './App';
 
 import { Provider } from 'react-redux'; // importing Provider to wrap the App component in
 import { createStore } from 'redux'; // importing creeateStore to create a store object
+import reducer from './reducers/reducer'; // importing the reducer to be the parameter for the createStore function
+
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
-const store = createStore();
+const store = createStore(reducer);
 
 ReactDOM.render(
     <Provider store={store}>
