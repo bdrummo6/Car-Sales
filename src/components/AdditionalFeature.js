f93705d02981a0c6e77d56a1061a231606dfe5e9
bdrummo6 <bradley.drummonds@gmail.com>
2020-05-02 16:37:10 -0400

    Update AdditionalFeature.js

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -8,7 +8,7 @@ const AdditionalFeature = props => {
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
       <button className="button" onClick={() => props.buyItem(props.feature)}>Add</button>
-      {props.feature.name} (+{props.feature.price})
+      {props.feature.name} (+${props.feature.price})
     </li>
   );
 };
