2698bcc0099e9a89d026d9ea5a9a7b5da897547b
bdrummo6 <bradley.drummonds@gmail.com>
2020-05-02 16:37:21 -0400

    Update actions.js

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -3,14 +3,14 @@ export const REMOVE_FEATURE = 'REMOVE_FEATURE'; // Export the REMOVE_FEATURE typ
 
 // Created am action function to buy an item
 export const buyItem = item => ({
-    // dipsatch an action here to add an item
+    // dispatch an action here to add an item
     type: BUY_ITEM,
     payload: item
 })
 
 // Created am action function to remove a feature
 export const removeFeature = item => ({
-    // dipsatch an action here to add an item
+    // dispatch an action here to remove a feature
     type: REMOVE_FEATURE,
     payload: item
 })
